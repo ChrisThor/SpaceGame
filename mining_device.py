bbe43b2d65bf666ef0703b6033b4d3369e96a13f
Krister Wolfhard <krister.wolfhard@it-designers.de>
2020-02-18 09:38:56 +0100

    Only recreate mining tool surface if zoom has changed

--- mining_device.py
+++ mining_device.py
@@ -7,17 +7,27 @@ class MiningDevice:
         self.size = size
         self.original_size = size
         self.mining_speed = speed
+        self.zoom = None
+        self.surface = None
         self.mode = 0
 
     def draw_affected_area(self, background, zoom, player, center_x, center_y):
-        surface = pygame.Surface((int(zoom * self.size), int(zoom * self.size)))
-        surface.fill((255, 50, 50))
-        surface.set_alpha(175)
+        if self.zoom is not None:
+            if self.zoom != zoom:
+                self.set_surface(zoom)
+        else:
+            self.set_surface(zoom)
 
         relative_distance_to_player = (self.position - player.position) * zoom
         screen_position = (center_x + relative_distance_to_player.x_value,
                            center_y + relative_distance_to_player.y_value + player.height * zoom / 2)
-        background.blit(surface, screen_position)
+        background.blit(self.surface, screen_position)
+
+    def set_surface(self, zoom):
+        self.surface = pygame.Surface((int(zoom * self.size), int(zoom * self.size)))
+        self.surface.fill((255, 50, 50))
+        self.surface.set_alpha(175)
+        self.zoom = zoom
 
     def update_position(self, position):
         self.position = position
